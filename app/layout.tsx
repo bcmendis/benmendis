0f34506996254125724c3aabdacf886bde4e208f
bcmendis <98670180+bcmendis@users.noreply.github.com>
2023-09-26 17:08:03 -0400

    Added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import NavBar from '@/components/layout/NavBar';
 import { Toaster } from '@/components/ui/toaster';
 import Providers from './providers';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: 'Ben Mendis',
@@ -18,20 +19,29 @@ interface RootLayoutProps {
 export default function RootLayout({children}: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
-        <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.variable
-          )}
-          >
-          <Providers>
-            <main className="min-h-screen antialiased">
-              <NavBar />
-              <section className="container py-6">{children}</section>
-              <Toaster />
-            </main>
-          </Providers>
-        </body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <Providers>
+          <main className="min-h-screen antialiased">
+            <NavBar />
+            <section className="container py-6">{children}</section>
+            <Toaster />
+          </main>
+        </Providers>
+        <Analytics
+          debug={true}
+          beforeSend={event => {
+            if (event.url.includes("/admin")) {
+              return null;
+            }
+            return event;
+          }}
+        />
+      </body>
     </html>
   );
 }
